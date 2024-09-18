c1c4eb8cb9cf3acc447ec6c6f13e90e3c4a26d27
Muhammed Sahad P <zanusahad@gmail.com>
2024-09-18 14:11:59 +0530

    Basic types in TypeScript

---
+++ BasicTypes/index.ts
@@ -0,0 +1,22 @@
+// BASIC TYPES
+  // 1. number
+  // 2. string
+  // 3. boolean
+  // 4. null
+  // 5. undefined
+  
+
+// 1. number
+let numb: number = 10;
+
+// 2. string
+let stri: string = "Hello";
+
+// 3. boolean
+let boool: boolean = true;
+
+// 4. null
+let nulll: null = null;
+
+// 5. undefined
+let undef: undefined = undefined;
