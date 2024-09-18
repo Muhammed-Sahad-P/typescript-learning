5f2c32f94ca11535d68f08baf5f8c982108679cc
Muhammed Sahad P <zanusahad@gmail.com>
2024-09-18 14:26:10 +0530

    inference and annotation

---
+++ Inference & Annotation/inference/inferAnnot.ts
@@ -0,0 +1,12 @@
+//Type Inference (implicit type inference)
+   // *TypeScript can automatically infer the type based on the assigned value.
+   //! eg; let username = "sachin";
+   //!     let age = 25;
+   //!     let isMarried = false;
+
+
+//Type Annotation (explicit type inference)
+  //*We can explicitly(manually) annotate the type of a variable   
+  //! eg; let username: string = "sachin";
+  //!     let age: number = 25;
+  //!     let isMarried: boolean = false;
